e4a1192d4f61464c39a51727e48b428a7b608806
vornao <migliorluca@gmail.com>
2021-04-01 00:03:16 +0200

    first beta 0.1.0

---
+++ main.py
@@ -0,0 +1,70 @@
+#                       ⋆ ˚｡⋆୨୧˚  v a p o r w a v e  b o t  ˚୨୧⋆｡˚ ⋆
+
+
+#Simple Telegram bot that converts standard unicode chars to  full-width ones
+#Unicode full width characters, means that all characters has the size of a chinese character.
+#Full width characters goes from 0xFF1 to 0xFFE5
+#Japanese hirigana characters goes from 0x3040 to 0x309f
+
+#⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ ⋆ ˚｡⋆୨୧˚ 
+
+import logging
+from telegram.inline.inlinequery import InlineQuery
+
+from telegram.inline.inlinequeryresult import InlineQueryResult
+import config
+import utils
+
+from uuid import uuid4
+from telegram import Update, ParseMode, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, InlineQueryHandler
+
+#enable logging 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+#initialize lists with characters 
+
+def main():
+    print("Welcome to vprwv-bot")
+    updater = Updater(config.BOT_TOKEN)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('help', help))
+    dispatcher.add_handler(InlineQueryHandler(inline_vaporize_query))
+
+    updater.start_polling()
+    updater.idle()
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        utils.start_msg,
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+def help(update, context):
+    update.message.reply_text(
+        utils.help_msg,
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+def inline_vaporize_query(update: Update, context: CallbackContext):
+    query = update.inline_query.query
+    
+    if query == '':
+        return
+
+    ans = [utils.hiramize(query), utils.emojize(query), utils.sparkleize(query) ]
+
+    results = [
+        InlineQueryResultArticle(
+            id=str(uuid4()), 
+            input_message_content=InputTextMessageContent(x),
+            title = x)
+            for x in ans
+        ]
+    update.inline_query.answer(results)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,59 @@
+import random
+
+start_msg = '\
+ₓ˚. ୭ ˚○◦˚.˚◦○˚ ୧ .˚ₓ\nWelcome to VaporwaveBot!\nₓ˚. ୭ ˚○◦˚.˚◦○˚ ୧ .˚ₓ\n\n\
+Usage: from your message input bar type:\n\n\
+***@vprwavebot your-text-here***\n\n\
+Send /help for more informations'
+
+help_msg = 'This is an inline bot, use it from the input bar from any chat!\n\n\
+*@vprwavebot type-here-your-message\n\n\
+*A prompt will now show, tap on the aesthetic quote you like\u2728\n\n\
+Under Construction!'
+
+
+
+ascii_akward = ['(ʘ_ʘ)', '(ʘ‿ʘ)', '(ಠ_ಠ)', 
+                '(◕_◕)', '(◕‿◕)', '\(◕ ◡ ◕\)', 
+                '(¬_¬)', 'ᶘ ᵒᴥᵒᶅ','(´・ω・｀)']
+sparkles = ['✧･ﾟ: *✧･ﾟ:*',  '*:･ﾟ✧*:･ﾟ✧']
+sparkles_two = 'ₓ˚. ୭ ˚○◦˚.˚◦○˚ ୧ .˚ₓ'
+galactic_one = ['｡･:*:･ﾟ★,｡･:*:･ﾟ☆', '｡･:*:･ﾟ★,｡･:*:･ﾟ☆']
+galactic_two = ['‧͙⁺˚*･༓☾', '☽༓･*˚⁺‧͙']
+emoji_sparkles = '\u2728'
+fullwidth = {i: chr(i + 0xFEE0) for i in range(0x21, 0x7F)}
+hiragana  = [chr(i) for i in range(0x3040, 0x309f)]
+
+def random_hiragana() -> str:
+    s = ''
+    for x in range(3):
+        s += random.choice(hiragana)
+    return s
+
+def vaporize(s: str) -> str:
+    vaporized = s.translate(fullwidth)
+    return vaporized
+
+def hiramize(s : str) -> str:
+    hiramized = vaporize(s)
+    hiramized += '  '
+    hiramized += random_hiragana()
+    return hiramized
+
+def emojize(s: str) -> str:
+    emojized = vaporize(s)
+    emojized += '  ' + random.choice(ascii_akward)
+    return emojized
+
+def sparkleize(s: str) -> str:
+    sparkleized = emoji_sparkles + '  '
+    sparkleized += vaporize(s) + '  '
+    sparkleized += emoji_sparkles
+    return sparkleized
+
+
+
+
+
+
+   
