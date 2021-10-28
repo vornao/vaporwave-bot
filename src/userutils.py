1df362a4e9d566a6febea58074b5e926cb267249
vornao <migliorluca@gmail.com>
2021-10-28 19:00:35 +0200

    fixed user log.

--- src/userutils.py
+++ src/userutils.py
@@ -4,7 +4,10 @@ import queue
 cache = []
 queue = queue.Queue()
 
+
+# uncomment for username logging make sure you have their consent
 def init_cache():
+    """
     try:
         with open(config.BOT_TOKEN + 'userlist.txt', "r") as f:
             l = f.read().splitlines()
@@ -12,13 +15,16 @@ def init_cache():
                 cache.append(line)
     except:
         print('file not exists')
+    """
+
 
 def usercheck():
+    """
     while True:
         user = queue.get()
         if user not in cache:
             cache.append(user)
             with open(config.BOT_TOKEN + 'userlist.txt', "a") as f:
                 f.write(user + '\n')
-
+    """
 
